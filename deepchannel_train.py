3c9d638ad2c937176416cd406b3e5ef2f5e36305
Kevin Ogden <ogdenkev@gmail.com>
2020-08-28 04:58:02 +0000

    Update deepchannel_train to load states as floats and specify what (amplitude) levels correspond to which state

--- deepchannel_train.py
+++ deepchannel_train.py
@@ -9,6 +9,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Train a deep learning network to detect events in single molecule data")
 parser.add_argument("train", metavar="TRAIN", nargs="+", help="Path(s) to the single molecule data for training. The file(s) must be in CSV format with three columns.")
+parser.add_argument("--levels", default="0,-5.4", help="A comma-separated list giving the amplitude level for each state [default: %(default)s]")
 parser.add_argument("--out", "-o", metavar="OUT", help="Path to save the trained model. By default the model will be saved to deepchannel-model-<TIMESTAMP>.h5")
 parser.add_argument("--scaler", help="Path to which the trained MinMaxScaler will be saved. By default, the scaler will be saved to deepchannel-scaler-<TIMESTAMP>.pkl")
 parser.add_argument("--history", help="Path to save the training history. By default, the history will be saved to deepchannel-training-history-<TIMESTAMP>.pkl")
@@ -18,6 +19,7 @@ args = parser.parse_args()
 import datetime
 import math
 import pickle
+from functools import partial
 
 import numpy as np
 import pandas as pd
@@ -52,13 +54,27 @@ def step_decay(epoch):
     return lrate
 
 
-def load_data(path):
-    return pd.read_csv(
+def _load_data(state_levels, path):
+    df = pd.read_csv(
         path,
         header=None,
         names=["time", "current", "state"],
-        dtype={"time": np.float64, "current": np.float64, "state": np.int32},
+        dtype={"time": np.float64, "current": np.float64, "state": np.float64},
     )
+    # df["state"] = df["state"].astype("category").cat.codes
+    states = np.ones(df.shape[0], dtype=np.int32)
+    state_in_levels = np.zeros(df.shape[0], dtype=bool)
+    for i, lev in enumerate(state_levels):
+        m = np.isclose(df["state"], lev)
+        if np.any(state_in_levels & m):
+            raise ValueError("Some of the states were similar to more than one of the amplitude levels")
+        state_in_levels |= m
+        states[m] = i
+    if not np.all(state_in_levels):
+        unmatched_levels = df.loc[~state_in_levels, "state"].sample(3).to_list()
+        raise ValueError(f"Some of the states, e.g. {unmatched_levels}, were not close to any of the levels")
+    df.loc[:, "state"] = states
+    return df
 
 
 '''
@@ -68,8 +84,10 @@ def load_data(path):
 batch_size = 256
 
 DATA_PATH = args.train  # "nmdar_data/NMDA_1.csv"
+state_levels = [float(x) for x in args.levels.split(",")]
+load_data = partial(_load_data, state_levels)
 
-print(f"Loading data from {DATA_PATH}")
+print(f"Loading data from {DATA_PATH} with state levels {state_levels}")
 dfs = map(load_data, DATA_PATH)
 train_data = pd.concat(dfs, axis=0, ignore_index=True)
 
